875517d063db0c629c8eb58320b62861fafe6420
hung <hungcs192@gmail.com>
2025-09-17 01:27:03 +0700

    Add solution for p1 two sum

---
+++ src/test/java/lc/p1_two_sum/SolutionTest.java
@@ -0,0 +1,24 @@
+package lc.p1_two_sum;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    public static Stream<Arguments> cases() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3}, 4, new int[]{0, 2})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    void twoSum(int[] arr, int target, int[] want) {
+        int[] actual = new Solution().twoSum(arr, target);
+        assertArrayEquals(want, actual);
+    }
+}
