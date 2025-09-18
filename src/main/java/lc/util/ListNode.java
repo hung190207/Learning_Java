98781a09cb2bbfe57a9ed4d6c1017d5557e88724
Hung <hungcs192@gmail.com>
2025-09-18 17:50:34 +0700

    Done problem

--- src/main/java/lc/util/ListNode.java
+++ src/main/java/lc/util/ListNode.java
@@ -8,4 +8,50 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+    // Convert ListNode into array.
+    public static ListNode fromArray(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return null;
+        ListNode head = new ListNode(arr[0]);
+        ListNode cur = head;
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+        return head;
+    }
+    // Convert ListNode to Integer Array
+    public static ListNode fromIntegerArray(Integer[] array) {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+
+        ListNode head = new ListNode(array[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != null) {
+                current.next = new ListNode(array[i]);
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+    // Convert ListNode to Array
+    public int[] toArray() {
+        java.util.List<Integer> list = new java.util.ArrayList<>();
+        ListNode current = this;
+
+        while (current != null) {
+            list.add(current.val);
+            current = current.next;
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    /*public void ListNodeArray(int[] arr, int val){
+        this.val = val;
+    }*/
 }
--- src/test/java/lc/p203_remove_linked_list_elements/SolutionTest.java
+++ src/test/java/lc/p203_remove_linked_list_elements/SolutionTest.java
@@ -1,6 +1,5 @@
 package lc.p203_remove_linked_list_elements;
 
-import com.sun.jdi.connect.Connector;
 import lc.util.ListNode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -8,18 +7,10 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SolutionTest {
     public static Stream<Arguments> test() {
-        ListNode node1 = new ListNode(1);
-        node1.next = new ListNode(2);
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        System.out.println();
         return Stream.of(
-                Arguments.of(head, 3, node1)
+                Arguments.of(ListNode.fromArray(new int[]{1, 2, 3}), 3, ListNode.fromArray(new int[]{1, 2}))
         );
     }
 
