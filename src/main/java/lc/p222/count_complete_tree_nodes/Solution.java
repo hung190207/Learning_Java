acff87fd438ffb2fb7bf2f9e47b5a22dd006d669
Hung <hungcs192@gmail.com>
2025-10-24 22:25:15 +0700

    Done problem.

--- src/main/java/lc/p222/count_complete_tree_nodes/Solution.java
+++ src/main/java/lc/p222/count_complete_tree_nodes/Solution.java
@@ -2,6 +2,7 @@ package lc.p222.count_complete_tree_nodes;
 
 import lc.util.TreeNode;
 
+// https://leetcode.com/problems/count-complete-tree-nodes/
 public class Solution {
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
