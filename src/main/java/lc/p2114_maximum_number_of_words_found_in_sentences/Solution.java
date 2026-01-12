a4ef6a1b9aff5981c09195ca0ce1480d6274c32a
hung <hungcs192@gmail.com>
2026-01-12 18:11:33 +0700

    Done problem.

--- src/main/java/lc/p2114_maximum_number_of_words_found_in_sentences/Solution.java
+++ src/main/java/lc/p2114_maximum_number_of_words_found_in_sentences/Solution.java
@@ -1,6 +1,6 @@
 package lc.p2114_maximum_number_of_words_found_in_sentences;
 
-//https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+// https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
 public class Solution {
     public int mostWordsFound(String[] sentences) {
         int res = 0;
